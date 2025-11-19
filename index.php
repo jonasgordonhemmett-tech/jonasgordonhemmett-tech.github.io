1bafc2afa51831cc308348b2d53e492d2e91db1b
Jonas Hemmett <jonasgordonhemmett@gmail.com>
2025-11-18 20:07:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Test Site</title>
+        <meta name="author" content="Jonas Hemmett">
+        <meta name="Jonas Hemmett's Site" content="My Site.">
+        <meta name="Jonas Hemmett's Site" content="My Site">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+<!--style went here -->
+    </head>
+
+<body>
+    <h1>Jonas Hemmett</h1>
+    <p>
+        <a href="https://www.linkedin.com/in/jonas-hemmett-8aa39a368">
+            LinkedIn
+        </a>
+        <a href="https://github.com/jonasgordonhemmett-tech">
+            Personal GitHub
+        </a>
+        <a href="github.com/Jonas-Hemmett">
+            UVM GitHub
+        </a>
+    </p>
+    <main>
+        <p>
+            This site is under development, please check back soon for updates.
+        </p>
+        <h2> Leather Work </h2>
+        <p>
+            <a href="https://www.etsy.com/shop/HemmettLeather">
+                Etsy
+            </a>
+        
+    </main>
+</body>
